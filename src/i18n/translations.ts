04c3877b7996b050d1129c7c1f02868ccea46be3
sebas <sebastianmq027@gmail.com>
2024-12-28 14:09:09 +0100

    add logo and change flags

--- src/i18n/translations.ts
+++ src/i18n/translations.ts
@@ -5,6 +5,8 @@ interface Translations {
     load: {
       title: string;
       subtitle: string;
+      dryTitle: string;
+      drySubtitle: string;
     };
     payment: {
       title: string;
@@ -15,11 +17,12 @@ interface Translations {
       washSubtitle: string;
       dryTitle: string;
       drySubtitle: string;
+      dryingProgram: string;
     };
     start: {
       title: string;
       subtitle: string;
-    };
+    }; 
   };
 }
 
@@ -29,7 +32,9 @@ export const translations: Translations = {
     dryingTitle: "Drying instructions",
     load: {
       title: "Load the washing machine and close the door.",
-      subtitle: "Remember to close pockets and zippers."
+      subtitle: "Remember to close pockets and zippers.",
+      dryTitle: "The drying process will continue",
+      drySubtitle:"You can increase the drying time before the end of the cycle: enter 1 euro for 10 minutes or 2 euros for 20 minutes."
     },
     payment: {
       title: "Make payment.",
@@ -39,19 +44,22 @@ export const translations: Translations = {
       washTitle: "Select wash program.",
       washSubtitle: "The display indicates the remaining process time.",
       dryTitle: "Select drying program.",
-      drySubtitle: "The display shows the remaining process time. You can open the door at any time to check the degree of drying of the laundry when you close the door press start"
+      drySubtitle: "The display shows the remaining process time. You can open the door at any time to check the degree of drying of the laundry when you close the door press start.",
+      dryingProgram: "Drying program"
     },
     start: {
       title: "Press start.",
       subtitle: "35 minute wash cycle."
-    }
+    },
   },
   de: {
     washingTitle: "Waschanleitung",
     dryingTitle: "Trocknungsanleitung",
     load: {
       title: "Waschmaschine beladen und Tür schließen.",
-      subtitle: "Denken Sie daran, Taschen und Reißverschlüsse zu schließen."
+      subtitle: "Denken Sie daran, Taschen und Reißverschlüsse zu schließen.",
+      dryTitle: "Der Trocknungsprozess wird fortgesetzt",
+      drySubtitle:"Sie können die Trocknungszeit vor dem Ende des Zyklus erhöhen: Geben Sie 1 Euro für 10 Minuten oder 2 Euro für 20 Minuten ein.",
     },
     payment: {
       title: "Zahlung vornehmen.",
@@ -61,11 +69,12 @@ export const translations: Translations = {
       washTitle: "Waschprogramm auswählen.",
       washSubtitle: "Das Display zeigt die verbleibende Prozesszeit an.",
       dryTitle: "Trocknungsprogramm auswählen.",
-      drySubtitle: "Das Display zeigt die verbleibende Prozesszeit an. Sie können die Tür jederzeit öffnen, um den Trocknungsgrad der Wäsche zu überprüfen. Nach dem Schließen der Tür Start drücken."
+      drySubtitle: "Das Display zeigt die verbleibende Prozesszeit an. Sie können die Tür jederzeit öffnen, um den Trocknungsgrad der Wäsche zu überprüfen. Nach dem Schließen der Tür Start drücken.",
+      dryingProgram: "Trocknungsprogramm"
     },
     start: {
       title: "Start drücken.",
       subtitle: "35-minütiger Waschzyklus."
-    }
+    },
   }
 };
